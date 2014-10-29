3cbc77b558b0be4b7283db18ab4bf894b47efca9
Julie Jarrett <jarrett.julieanna@gmail.com>
2014-10-29 15:53:02 -0400

    stormpath

--- myapp/stormpath-express-sample/app.js
+++ myapp/stormpath-express-sample/app.js
@@ -1,25 +1,15 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// These are the new imports we're adding:
-var passport = require('passport');
-var StormpathStrategy = require('passport-stormpath');
-var session = require('express-session');
-var flash = require('connect-flash');
-
-var index_routes = require('./routes/index');
-var auth_routes = require('./routes/auth');
-
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
-var strategy = new StormpathStrategy();
-passport.use(strategy);
-passport.serializeUser(strategy.serializeUser);
-passport.deserializeUser(strategy.deserializeUser);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,21 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(session({
-  secret: process.env.EXPRESS_SECRET,
-  key: 'sid',
-  cookie: {secure: false},
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-app.use('/', index_routes);
-app.use('/', auth_routes);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
