a1d17f70a85a17736f25733b40b671b28e4cdefc
Julie Jarrett <jarrett.julieanna@gmail.com>
2014-10-29 14:48:19 -0400

    add bootstrap

--- myapp/stormpath-express-sample/app.js
+++ myapp/stormpath-express-sample/app.js
@@ -11,11 +11,15 @@ var StormpathStrategy = require('passport-stormpath');
 var session = require('express-session');
 var flash = require('connect-flash');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index_routes = require('./routes/index');
+var auth_routes = require('./routes/auth');
+
 
 var app = express();
-
+var strategy = new StormpathStrategy();
+passport.use(strategy);
+passport.serializeUser(strategy.serializeUser);
+passport.deserializeUser(strategy.deserializeUser);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,8 +33,18 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+app.use(session({
+  secret: process.env.EXPRESS_SECRET,
+  key: 'sid',
+  cookie: {secure: false},
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use('/', index_routes);
+app.use('/', auth_routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
